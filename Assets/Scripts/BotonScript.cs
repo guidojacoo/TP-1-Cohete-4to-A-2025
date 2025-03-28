a478f3716ff9e08563b74595c330c81aca4c6753
guido jacofsky <166538636+guidojacoo@users.noreply.github.com>
2025-03-28 08:56:43 -0300

    mm

---
+++ Assets/Scripts/BotonScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonScript : MonoBehaviour
+{
+    public GameObject BotonGO;
+    private void OnMouseDown()
+    {
+        BotonGO.SetActive(false);
+    }
+    private void OnMouseUp()
+    {
+        BotonGO.SetActive(true);
+    }
+}
